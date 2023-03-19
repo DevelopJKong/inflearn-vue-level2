793fc62ffcb625c2d65b6a7f4d372a2cea7ee028
cafe small house <tbaram10@naver.com>
2023-03-19 17:47:14 +0900

    TodoHeader 컴포넌트 구현 완료

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <div>hello</div>
   <TodoHeader></TodoHeader>
   <TodoInput></TodoInput>
   <TodoList></TodoList>
@@ -22,4 +21,22 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,9 +1,19 @@
 <template>
   <!-- HTML -->
-  <div>header</div>
+  <header>
+    <h1>To Do it!</h1>
+  </header>
 </template>
 <script>
-console.log("hello");
+// JavaScript
+console.log("todo header");
 export default {};
 </script>
-<style></style>
+<style scoped>
+/* CSS */
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
